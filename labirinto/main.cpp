dc677863971db99a166e0b0532086866020e4242
Daniel Alves Fonseca Neto <111474772+DanielAlvesFonsecaNeto@users.noreply.github.com>
2024-09-15 22:39:04 -0300

    começando a contruir a logica do labirinto

--- labirinto/main.cpp
+++ labirinto/main.cpp
@@ -1,36 +1,51 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 #include <vector>
+#include <utility>
+#include <stack>
+
+/* ---------------------------------CORES----------------------------------- */
+
+// cor do fundo
+SDL_Color cor_fundo = {120, 120, 180, 255}; // meio azul
+// cor da parede
+SDL_Color cor_parede = {20, 20, 20, 255}; // quase preto
+// cor do caminho
+SDL_Color cor_caminho = {130, 130, 160, 255}; // quase branco cinza azulado
+
+/* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^CORES^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ */
 
 /* ---------------------------------VARIAVEIS E ESTRTURAS GLOBAIS----------------------------------- */
 
-
+// tamanho da janela
 const int Janela_L = 720;
 const int Janela_A = 720;
 
-const int dim_matriz = 23; // quantas celulas ^2 tem a matriz
+// quantas celulas ^2 tem a matriz, nxn
+const int dim_matriz = 23;
 
 // celula de uma matriz , tem informações como direção de caminho etc..
 struct labirinto_celula
 {
-    int x, y;
+    // verificação se ja foi visitado para poder entrar na pilha ou não
+    bool visitado = false;
+    // cordenadas caso precise
+    std::pair<int, int> cordenadas;
+    // quem era o pai, util para achar menor caminho
+    std::pair<int, int> pai_cordenadas;
+    // tem caminho ? 0 = não, 1 = sim
+    int norte = 0;
+    int sul = 0;
+    int leste = 0;
+    int oeste = 0;
+    // caso precise para manipulações melhores do grafo
+    std::vector<std::pair<int, int>> ligacoes;
 };
 
+// matriz do labirinto, essa é a estrura que vai ser processada e armazenar o labirinto
 std::vector<std::vector<labirinto_celula>> matriz_labirinto;
 
-
 /* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^VARIAVEIS E ESTRTURAS GLOBAIS^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ */
-
-/* ---------------------------------CORES----------------------------------- */
-
-//cor do fundo
-SDL_Color cor_fundo = {120,120,180,255}; // meio azul
-//cor da parede
-SDL_Color cor_parede = {20,20,20,255}; // quase preto
-//cor do caminho
-SDL_Color cor_caminho = {130,130,160,255}; // quase branco cinza azulado
-
-/* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^CORES^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ */
 
 /*
 Função que desenha malha de quadrados na tela, seria o labirinto sem caminho, apenas paredes
@@ -39,7 +54,7 @@ e desenhar cada quadrado da grid
 void desenharQuadradoComOutroDentro(SDL_Renderer *renderizador, int offsetX, int offsetY, int x, int y, int tamanho)
 {
     // Desenhar o quadrado maior, levando em consideração o offset
-    SDL_Rect quadradoMaior = {offsetX , offsetY , tamanho, tamanho};
+    SDL_Rect quadradoMaior = {offsetX, offsetY, tamanho, tamanho};
     SDL_SetRenderDrawColor(renderizador, cor_parede.r, cor_parede.g, cor_parede.b, cor_parede.a); // verde
     SDL_RenderFillRect(renderizador, &quadradoMaior);
 
@@ -56,7 +71,6 @@ void desenharQuadradoComOutroDentro(SDL_Renderer *renderizador, int offsetX, int
     SDL_RenderFillRect(renderizador, &quadradoMenor);
 }
 
-
 void desenharGrids(SDL_Renderer *renderizador, int rect_size, int offsetX, int offsetY)
 {
     // Itera sobre as células da matriz
@@ -68,7 +82,7 @@ void desenharGrids(SDL_Renderer *renderizador, int rect_size, int offsetX, int o
             int x = offsetX + j * rect_size;
             int y = offsetY + i * rect_size;
 
-            desenharQuadradoComOutroDentro(renderizador,x,y,0,0,rect_size);
+            desenharQuadradoComOutroDentro(renderizador, x, y, 0, 0, rect_size);
         }
     }
 
@@ -76,8 +90,53 @@ void desenharGrids(SDL_Renderer *renderizador, int rect_size, int offsetX, int o
 }
 
 /* função que constroi o labirinto e tambem desenha */
-void construir_Labirinto(){
+void construir_Labirinto()
+{
 
+    srand(time(nullptr));
+
+    bool continuar_loop = true;
+
+    std::stack<std::pair<int, int>> pilha;
+    pilha.push({0, 0}); // começa a geração do labirinto a partir da celula 0,0 , a primeira
+    matriz_labirinto[0][0].visitado = true;
+    matriz_labirinto[0][0].cordenadas = {0, 0};
+    matriz_labirinto[0][0].norte = 1;               // seria o caminho pra saida do labirinto
+    matriz_labirinto[0][0].pai_cordenadas = {0, 0}; // ele mesmo
+
+    while (!pilha.empty() && continuar_loop)
+    {
+
+        // desempilha da pilha, vamos trabalhar com o atual
+        std::pair<int, int> atual = pilha.top();
+        pilha.pop();
+
+        // verifica se ja foi visitado
+        if (matriz_labirinto[atual.first][atual.second].visitado == false)
+        {
+
+            // definindo direções possiveis
+            // norte
+            std::pair<int, int> norte_do_atual = {atual.first - 1, atual.second};
+            // sul
+            std::pair<int, int> sul_do_atual = {atual.first + 1, atual.second};
+            // leste
+            std::pair<int, int> leste_do_atual = {atual.first, atual.second + 1};
+            // oeste
+            std::pair<int, int> oeste_do_atual = {atual.first, atual.second - 1};
+
+            /*
+                para facilitar as operações com rng e listas vou definir que
+                :
+                    0 = norte
+                    1 = sul
+                    2 = leste
+                    3 = oeste
+            */
+
+            // validando movimentos com IF
+        }
+    }
 }
 
 int main(int argc, char *argv[])
